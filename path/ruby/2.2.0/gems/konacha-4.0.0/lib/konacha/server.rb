a917eda37c1b153ee4611c1f94454d91dffcffc5
Mathias Keus <mathias.keus@phz.fi>
2016-03-13 16:35:03 +0200

    .gitignore is now working

--- path/ruby/2.2.0/gems/konacha-4.0.0/lib/konacha/server.rb
+++
@@ -1,7 +0,0 @@
-module Konacha
-  class Server
-    def self.start
-      Rack::Server.start(:app => Konacha.application, :Host => Konacha.host, :Port => Konacha.port, :AccessLog => [])
-    end
-  end
-end
